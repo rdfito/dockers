022eecc7d59a5930fbcd6498cd365ab9d556a5d3
rdfito <fitooo@gmail.com>
2020-05-07 01:04:23 +0200

    DB privilege issues fixed

--- pg10/initdb/01_db.sh
+++ pg10/initdb/01_db.sh
@@ -5,8 +5,9 @@ set -e
 # w: --no-password
 # d: --dbname
 
-if [ -n "$DB_NAME" ]; then
+if [ -n "$DB_NAME" ] && [ -n "$DB_USER_NAME" ] && [ -n "$DB_USER_PASSWORD" ]; then
 psql -U "$POSTGRES_USER" -w -d "$POSTGRES_DB" <<-EOSQL
+    CREATE USER "$DB_USER_NAME" WITH PASSWORD '$DB_USER_PASSWORD';
     CREATE DATABASE "$DB_NAME";
 EOSQL
 fi
